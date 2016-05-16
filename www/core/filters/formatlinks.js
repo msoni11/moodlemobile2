b72ac937b14f6b7a88f75151aceb2617175d6aa8
Mahendra Soni <mmahendra.soni@gmail.com>
2016-05-16 13:31:08 +0530

    GitHub #6: Remove console log

--- www/core/filters/formatlinks.js
+++ www/core/filters/formatlinks.js
@@ -7,7 +7,6 @@ angular.module('mm.core')
  * @name mmFormatLinks
  */
 .filter('mmFormatLinks', function($mmSite) {
-    console.log($mmSite.getURL());
     var pattern = new RegExp('(src=")(?!https?:\/\/)\/?([^"]+\.(jpe?g|png|gif|bmp))"', "ig");
     return function(text) {
         return text.replace(pattern, '$1' + $mmSite.getURL() + '/$2"');
