99483767fbe20e94bc6a06b50bb4ff71b6c2763f
Mahendra Soni <mmahendra.soni@gmail.com>
2016-05-16 13:26:28 +0530

    GitHub #6: Broken images issue fixed for mobile app.

---
+++ www/core/filters/formatlinks.js
@@ -0,0 +1,15 @@
+angular.module('mm.core')
+
+/**
+ * Filter to search URLs that are absolute and make them relative.
+ *
+ * @module mm.core
+ * @name mmFormatLinks
+ */
+.filter('mmFormatLinks', function($mmSite) {
+    console.log($mmSite.getURL());
+    var pattern = new RegExp('(src=")(?!https?:\/\/)\/?([^"]+\.(jpe?g|png|gif|bmp))"', "ig");
+    return function(text) {
+        return text.replace(pattern, '$1' + $mmSite.getURL() + '/$2"');
+    };
+});
